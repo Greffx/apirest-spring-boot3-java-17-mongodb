31e0126393bc7e6ae56347ae9054479981562ef6
Eduardo Greff <eduardogreff0@gmail.com>
2023-01-04 17:35:21 -0300

    Post controller and service

---
+++ src/main/java/com/eduardogreff/workshopmongo/controllers/PostController.java
@@ -0,0 +1,23 @@
+package com.eduardogreff.workshopmongo.controllers;
+
+import com.eduardogreff.workshopmongo.entities.Post;
+import com.eduardogreff.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+}
